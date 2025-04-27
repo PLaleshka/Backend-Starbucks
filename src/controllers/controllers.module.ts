bda6d6a8e73a31e98e7636e548de1b2ab6f0f0f8
JoseEscalanteA <josedc1601@gmail.com>
2025-04-27 19:07:53 -0400

    Se actualizo controllers.module.ts

--- src/controllers/controllers.module.ts
+++ src/controllers/controllers.module.ts
@@ -3,17 +3,38 @@ import { DatabaseModule } from './database/database.module';
 import { ClienteService } from 'src/providers/cliente/cliente.service';
 import { PedidoService } from 'src/providers/pedido/pedido.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Cliente } from 'src/controllers/database/entities/cliente.entity.ts';
-import { Pedido } from 'src/controllers/database/entities/pedido.entity.ts';
+import { Cliente } from 'src/controllers/database/entities/cliente.entity';
+import { Pedido } from 'src/controllers/database/entities/pedido.entity';
 import { PedidoController } from 'src/controllers/pedido/pedido.controller';
 import { ClienteController } from 'src/controllers/cliente/cliente.controller';
+import { Administrador } from './database/entities/administrador.entity';
+import { Tienda } from './database/entities/tienda.entity';
+import { Barista } from './database/entities/barista.entity';
+import { Producto } from './database/entities/producto.entity';
+import { DetallePedido } from './database/entities/detalle-pedido.entity';
+import { AdministradorController } from './administrador/administrador.controller';
+import { TiendaController } from './tienda/tienda.controller';
+import { BaristaController } from './barista/barista.controller';
+import { ProductoController } from './producto/producto.controller';
+import { DetallePedidoController } from './detalle-pedido/detalle-pedido.controller';
+import { AdministradorService } from 'src/providers/administrador/administrador.service';
+import { TiendaService } from 'src/providers/tienda/tienda.service';
+import { BaristaService } from 'src/providers/barista/barista.service';
+import { ProductoService } from 'src/providers/producto/producto.service';
+import { DetallePedidoService } from 'src/providers/detalle-pedido/detalle-pedido.service';
 
 @Module({
   imports: [
     //DatabaseModule
-    TypeOrmModule.forFeature([Cliente, Pedido])  
+    TypeOrmModule.forFeature([Cliente, Pedido, Administrador, Tienda, Barista,
+      Producto, DetallePedido
+    ])  
   ],
-  controllers: [ClienteController, PedidoController],
-  providers: [ClienteService, PedidoService],
+  controllers: [ClienteController, PedidoController, AdministradorController,
+    TiendaController, BaristaController, ProductoController, DetallePedidoController
+  ],
+  providers: [ClienteService, PedidoService, AdministradorService,
+    TiendaService, BaristaService, ProductoService, DetallePedidoService
+  ],
 })
 export class ControllersModule {}
