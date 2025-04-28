57bc4a88cc76efcb5b2633f608fe142ba2b5ceb0
PLaleshka <polettemontt@gmail.com>
2025-04-27 22:13:51 -0400

    Agregando avance de cliente y login

---
+++ src/controllers/login/login.controller.ts
@@ -0,0 +1,21 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { LoginRequestDTO } from './dto/login-request.dto';
+import { LoginResponseDTO } from './dto/login-response.dto';
+import { LoginService } from 'src/providers/login/login.service';
+import { RegisterRequestDTO } from './dto/register-request.dto';
+
+
+@Controller('login')
+export class LoginController {
+    constructor(private readonly loginService: LoginService) {}
+
+    @Post()
+    public async login(@Body() request: LoginRequestDTO): Promise<LoginResponseDTO> {
+        return await this.loginService.validate(request);
+    }
+
+    @Post('register')
+    public async register(@Body() request: RegisterRequestDTO): Promise<any> {
+        return this.loginService.register(request);
+    }
+}
---
+++ src/providers/login/login.service.ts
@@ -0,0 +1,58 @@
+import { Injectable, HttpException } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Cliente } from "src/controllers/database/entities/cliente.entity.ts";
+import { Repository } from "typeorm";
+import { hash, compare} from "bcrypt";
+import { RegisterRequestDTO } from "src/controllers/login/dto/register-request.dto";
+import { LoginRequestDTO } from "src/controllers/login/dto/login-request.dto";
+import { RegisterResponseDTO } from "src/controllers/login/dto/register-response.dto";
+import { LoginResponseDTO } from "src/controllers/login/dto/login-response.dto";
+
+@Injectable()
+export class LoginService {
+    constructor(
+        @InjectRepository(Cliente)
+        private readonly clienteRepository: Repository<Cliente>,
+    ) {}
+
+    public async register(request: RegisterRequestDTO): Promise<RegisterResponseDTO> {
+        const { contraseña } = request;
+        const encryptedPassword = await hash(contraseña, 10);
+
+        request = { ...request, contraseña: encryptedPassword };
+
+        const cliente: Cliente = new Cliente();
+        Object.assign(cliente, request);
+        const result: Cliente = this.clienteRepository.create(cliente);
+
+        await this.clienteRepository.save(result);
+
+        const response: RegisterResponseDTO = { status: "Cliente registrado" } as RegisterResponseDTO;
+
+        return response;
+    }
+
+    public async validate(credentials: LoginRequestDTO): Promise<LoginResponseDTO> {
+        try {
+            const { correoElectronico } = credentials;
+            const result = await this.clienteRepository.findOneBy({ correoElectronico });
+
+            if (!result) {
+                throw new HttpException("Cliente no encontrado", 404);
+            }
+
+            const { contraseña } = credentials;
+            const checkPassword: boolean = await compare(contraseña, result.contraseña);
+
+            if (!checkPassword) 
+                new HttpException("Contraseña incorrecta", 403);
+
+            const response: LoginResponseDTO = { 
+                status: "Cliente autenticado",
+            } as LoginResponseDTO;
+            return response;
+        } catch (error: any) {
+            throw new Error(error);
+        }
+    }
+}
