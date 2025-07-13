d64d75f87d3f502154f4a365c46863737cd92534
Bastian <bastiansucsochoque@gmail.com>
2025-07-13 00:03:27 -0400

    agregado guards, funcionado perfecto

--- src/controllers/login/login.controller.ts
+++ src/controllers/login/login.controller.ts
@@ -1,10 +1,13 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { LoginService } from 'src/providers/login/login.service';
 import { LoginRequestDTO } from './dto/login-request.dto';
 import { RegisterRequestDTO } from './dto/register-request.dto';
 import { RegisterWithRoleDTO } from './dto/register-with-role.dto';
 import { LoginResponseDTO } from './dto/login-response.dto';
 import { RegisterResponseDTO } from './dto/register-response.dto';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import { RolesGuard } from '../auth/roles.guard';
+import { Roles } from '../auth/roles.decorator';
 
 @Controller('login')
 export class LoginController {
@@ -16,6 +19,8 @@ export class LoginController {
     }
 
     @Post('register-with-role')
+    @UseGuards(JwtAuthGuard, RolesGuard)
+    @Roles('administrador')
     async registerWithRole(@Body() body: RegisterWithRoleDTO): Promise<RegisterResponseDTO> {
         return this.loginService.registerWithRole(body);
     }
