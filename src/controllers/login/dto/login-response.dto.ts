3c8a25f9c503fa93deb3389629097f0194764fd8
PLaleshka <polettemontt@gmail.com>
2025-07-24 23:17:51 -0400

    Update login-response.dto.ts

--- src/controllers/login/dto/login-response.dto.ts
+++ src/controllers/login/dto/login-response.dto.ts
@@ -1,4 +1,15 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class LoginResponseDTO {
+  @ApiProperty({
+    example: 'success',
+    description: 'Estado de la operación de login',
+  })
   status!: string;
+
+  @ApiProperty({
+    example: 'eyJhbGc...',
+    description: 'Token JWT generado al iniciar sesión',
+  })
   access_token!: string;
 }
