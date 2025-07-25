f62fa61d5c55dfb3b1ee2db099d20345bc9db290
PLaleshka <polettemontt@gmail.com>
2025-07-24 23:34:56 -0400

    Update ProductoUpdateDTO.ts

--- src/controllers/producto/dto/ProductoUpdateDTO.ts
+++ src/controllers/producto/dto/ProductoUpdateDTO.ts
@@ -1,18 +1,37 @@
 import { IsString, IsNumber, IsOptional } from 'class-validator';
+import { ApiPropertyOptional } from '@nestjs/swagger';
+import { Type } from 'class-transformer';
 
 export class ProductoUpdateDTO {
+  @ApiPropertyOptional({
+    example: 'Café Espresso',
+    description: 'Nuevo nombre del producto',
+  })
   @IsString()
   @IsOptional()
   nombre?: string;
 
+  @ApiPropertyOptional({
+    example: 'Bebida caliente',
+    description: 'Nueva categoría del producto',
+  })
   @IsString()
   @IsOptional()
   categoria?: string;
 
-  @IsNumber()
+  @ApiPropertyOptional({
+    example: 9.99,
+    description: 'Nuevo precio del producto',
+  })
   @IsOptional()
+  @Type(() => Number) // Esto asegura que el valor se transforme correctamente
+  @IsNumber()
   precio?: number;
 
+  @ApiPropertyOptional({
+    example: 'Café negro fuerte',
+    description: 'Nueva descripción del producto',
+  })
   @IsString()
   @IsOptional()
   descripcion?: string;
