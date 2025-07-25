9bed2d528a359f96ad9ee3783bba1ab5103eaea4
PLaleshka <polettemontt@gmail.com>
2025-07-24 23:00:19 -0400

    Update IPostIngredienteRequest.ts

--- src/controllers/ingrediente/dto/IPostIngredienteRequest.ts
+++ src/controllers/ingrediente/dto/IPostIngredienteRequest.ts
@@ -1,11 +1,22 @@
 import { IsNotEmpty, IsString, MaxLength } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class IPostIngredienteRequest {
+  @ApiProperty({
+    example: 'Café en grano',
+    description: 'Nombre del ingrediente',
+    maxLength: 100,
+  })
   @IsNotEmpty()
   @IsString()
   @MaxLength(100)
   nombre!: string;
 
+  @ApiProperty({
+    example: 'Ingredientes seleccionados para espresso',
+    description: 'Descripción del ingrediente',
+    maxLength: 255,
+  })
   @IsNotEmpty()
   @IsString()
   @MaxLength(255)
