a7982edd5089dd1c0ca988dc8fb8e5ea2bb4fb69
PLaleshka <polettemontt@gmail.com>
2025-07-24 23:05:24 -0400

    Update ingrediente.dto.ts

--- src/controllers/ingrediente/dto/ingrediente.dto.ts
+++ src/controllers/ingrediente/dto/ingrediente.dto.ts
@@ -1,4 +1,24 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString, MaxLength } from 'class-validator';
+
 export class IngredienteDTO {
+  @ApiProperty({
+    example: 'Café en grano',
+    description: 'Nombre del ingrediente',
+    maxLength: 100,
+  })
+  @IsNotEmpty()
+  @IsString()
+  @MaxLength(100)
   nombre!: string;
+
+  @ApiProperty({
+    example: 'Ingredientes seleccionados para espresso',
+    description: 'Descripción detallada del ingrediente',
+    maxLength: 255,
+  })
+  @IsNotEmpty()
+  @IsString()
+  @MaxLength(255)
   descripcion!: string;
 }
