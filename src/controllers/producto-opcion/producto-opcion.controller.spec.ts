297faaddcf44b842a7d886468175b68b8ade49ec
PLaleshka <polettemontt@gmail.com>
2025-07-27 01:44:54 -0400

    Update producto-opcion.controller.spec.ts

--- src/controllers/producto-opcion/producto-opcion.controller.spec.ts
+++ src/controllers/producto-opcion/producto-opcion.controller.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ProductoOpcionController } from 'src/controllers/producto-opcion/producto-opcion.controller';
+import { ProductoOpcionService } from 'src/providers/producto-opcion/producto-opcion.service'; // ajusta si la ruta cambia
 
 describe('ProductoOpcionController', () => {
   let controller: ProductoOpcionController;
@@ -7,6 +8,12 @@ describe('ProductoOpcionController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ProductoOpcionController],
+      providers: [
+        {
+          provide: ProductoOpcionService,
+          useValue: {}, // simulamos el servicio con un mock vacío
+        },
+      ],
     }).compile();
 
     controller = module.get<ProductoOpcionController>(ProductoOpcionController);
