4a4650f00da1ec43ce4ad82c6f6180e4f7098070
PLaleshka <polettemontt@gmail.com>
2025-07-27 01:45:37 -0400

    Update opcion-personalizacion.service.spec.ts

--- src/providers/opcion-personalizacion/opcion-personalizacion.service.spec.ts
+++ src/providers/opcion-personalizacion/opcion-personalizacion.service.spec.ts
@@ -1,12 +1,20 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { OpcionPersonalizacionService } from './opcion.personalizacion.service';
+import { OpcionPersonalizacion } from 'src/controllers/database/entities/opcion-personalizacion.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
 
 describe('OpcionPersonalizacionService', () => {
   let service: OpcionPersonalizacionService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [OpcionPersonalizacionService],
+      providers: [
+        OpcionPersonalizacionService,
+        {
+          provide: getRepositoryToken(OpcionPersonalizacion),
+          useValue: {}, // mock del repositorio
+        },
+      ],
     }).compile();
 
     service = module.get<OpcionPersonalizacionService>(OpcionPersonalizacionService);
